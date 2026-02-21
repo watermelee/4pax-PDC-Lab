9d603148c99e07f325ec3bc9078e07127ad35ede
Threcia <cabuguason.threcia@gmail.com>
2026-02-21 01:50:32 +0000

    Implement Sequential Version

--- Second Group Act - Analog to Digital/lab.py
+++ Second Group Act - Analog to Digital/lab.py
@@ -6,3 +6,11 @@ def grade(submission):
     time.sleep(0.1)  # simulate grading time
     return submission * 2
 
+def run_sequential(students):
+    start = time.time()
+
+    for s in students:
+        grade(s)
+
+    end = time.time()
+    return end - start
